698f78f8599f4d7afb8ed0cc8ba881f71955e099
JF31281 <JF31281@LPAA000698>
2024-03-07 09:30:29 +0200

    ....

--- signin.js
+++ signin.js
@@ -95,19 +95,16 @@ $(document).ready(function () {
 
     function findErrors() {
         $errors.each(function (ind, el) {
-            if (el.length > 0) {
-                console.log(el);
-                console.log('element');
-            };
             if (ind > 0) {
                 console.log(ind);
                 console.log('index');
+                errorMessages.realMsg.val(errorMessages.fakeMsg.val());
             }
-            $('.error').on('change', function (pind, pel) {
-                $errors.removeAttr("aria-hidden");
-                var text = $(pel).text();
-                console.log(text);
-            });
+            // $('.error').on('change', function (pind, pel) {
+            //     $errors.removeAttr("aria-hidden");
+            //     var text = $(pel).text();
+            //     console.log(text);
+            // });
         });
     }
 
